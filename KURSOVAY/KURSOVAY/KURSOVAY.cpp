a96a419d57273d76952b67b7b2967d7d901deea2
Programmer Nedo <sabaliigor@gmail.com>
2021-03-31 00:26:23 +0300

    v.2.8
    finish Kursovay, fix all bugs

--- KURSOVAY/KURSOVAY/KURSOVAY.cpp
+++ KURSOVAY/KURSOVAY/KURSOVAY.cpp
@@ -6,13 +6,42 @@
 #include <stdio.h>
 #include <string.h>
 #include <math.h>
+#include <algorithm>
 using namespace std;
+
 
 bool TryParse(const string& symbol);
 int Priority(const string& c);
 bool isOperator(const string& c);
 vector<string> StringToRPN(string infix);
 
+int check_bkl(string infix) {
+    int status = 0;
+    int left = 0;
+    for (int i = 0; i < infix.size(); i++)
+        if (infix[i] == '(') left++;
+
+    int right = 0;
+    for (int i = 0; i < infix.size(); i++)
+        if (infix[i] == ')') right++;
+
+    if (left == right)
+    {
+        status = 1;
+    }
+    else
+    {
+        cout << "Incorect enter, Reenter expresion:" << endl;
+        if (left > right) {
+            cout << "the number of brackets on the left is greater than on the right" << endl;
+        }
+        else
+            cout << "the number of brackets on the right is greater than on the left" << endl;
+        system("pause");
+    }
+
+    return status;
+}
 
 bool is_sign(string str) {
     std::string plus = "+";
@@ -34,8 +63,6 @@ bool is_sign(string str) {
     else
         return false;
 }
-
-
 
 float Opertion(string str,float a, float b) {
     std::string plus="+";
@@ -111,63 +138,149 @@ vector<string> count_RPN(vector<string> outputList){
     return outputList;
 }
 
+vector<string> StrToVector(string input) {
+    std::stringstream ss(input);
+    
+    std::istream_iterator<std::string> begin(ss);
+    std::istream_iterator<std::string> end;
+    std::vector<std::string> vstrings(begin, end);
+    std::copy(vstrings.begin(), vstrings.end(), std::ostream_iterator<std::string>(std::cout, "\n"));
+    system("cls");
+    return vstrings;
+}
+
+string deleteSymbolFromString(string input,char symbol) {
+    input.erase(std::remove(input.begin(), input.end(), symbol), input.end());
+    return input;
+}
+
+string inputString() {
+    string temp;
+    string fullString;
+    std::string equal = "=";
+    for (int out = 0; out != 1;)
+    {
+        getline(cin, temp);
+        fullString = fullString + ' ' + temp;
+        if (temp.find(equal) != string::npos) {
+            out++;
+        }   
+    }
+    return fullString;
+}
+
+bool checkDoubleDot(string input) {
+    int size = input.size();
+    short dot = 0;
+    for (int i = 0; i < size; i++)
+    {
+        if (input[i] == '.') {
+            dot++;
+        }
+    }
+    if (dot > 1) {
+        cout << input << " have two or more dots"<<endl;
+        return false;
+    }
+    return true;
+}
+
+bool checkSign(string input) {
+    if (input[0] == '+') {
+        return true;
+    }
+    else if (input[0] == '-') {
+        return true;
+    }
+    else if (input[0] == '*') {
+        return true;
+    }
+    else if (input[0] == '/') {
+        return true;
+    } 
+    
+    else
+        
+        return false;
+}
+
+
+
+bool checkLetters(string input) {
+    int size = input.size();
+
+    for (int i = 0; i < size; i++)
+    {
+        if ((!isdigit(input[i]) && input[i]!='.' && input[0] != '(' && input[0] != ')') && !checkSign(input)) {
+
+            cout << input << " have extra letter" << endl;
+            return false;
+        }
+    }
+   
+    return true;
+}
+
+bool checkExtraSignAndDigit(vector<string> inputVector){
+
+    inputVector.erase(std::remove(inputVector.begin(), inputVector.end(), ")"), inputVector.end());
+    inputVector.erase(std::remove(inputVector.begin(), inputVector.end(), "("), inputVector.end());
+
+    for (int i = 0; i < inputVector.size() - 1; ++i) {
+        string temp = inputVector[i];
+        string temp2 = inputVector[i+1];
+        if (checkSign(temp) == checkSign(temp2)) {
+            cout << temp << " " << temp2 << " is not corect input, you miss sign or number" << endl;
+            return false;
+        }
+    }
+    return true;
+}
+
+bool checkExtraChar(vector<string> inputVector) {
+
+    for (int i = 0; i < inputVector.size(); ++i) {
+        string temp = inputVector[i];
+        for (int step = 0; step < temp.size(); step++)
+        {
+            if (!checkDoubleDot(temp)) {
+                return false;
+            }
+            if (!checkLetters(temp)) {
+                return false;
+            }
+            if (!checkExtraSignAndDigit(inputVector)) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+
 
 
 int main()
 {
-    string infix;// = "12.3 * 3 + 1 * 0 + 2 / 0 + ( 5 * 1 ) / 0 + 1";//our infix expression
+    string infix;// = "12.3 * 3 + 1 * 0 + 2 / 0 + ( ( 5 * 1 ) + 1 ) / 0 + 1=";//our infix expression
 
-    for (int status = 0; status != 1 ; )
+    for (int status = 0; status != 2 ; )
     {
         cout << "Enter expresion, after every symbol put space \"( ( a + b ) * c )\":" << endl;
-        getline(cin, infix);
-        int left = 0;
-        for (int i = 0; i < infix.size(); i++)
-            if (infix[i] == '(') left++;
-
-        int right = 0;
-        for (int i = 0; i < infix.size(); i++)
-            if (infix[i] == ')') right++;
-
-        if (left==right)
-        {
-            status = 1;
+        infix = deleteSymbolFromString(inputString(),'=');
+        status=check_bkl(infix);
+        if (status == 1) {
+            vector<string> temp = StrToVector(infix);
+            if (checkExtraChar(temp)) {
+                status++;
+            }
         }
-        else
-        {
-            cout << "Incorect enter, Reenter expresion:" << endl;
-            system("pause");
-        }
-
         system("cls");
     }
     
     vector<string> outputList = StringToRPN(infix);
 
-    for (int status = 0; status != 1; )
-    {
 
-        int elements = outputList.size();
-
-        int sign = 0;
-        for (int i = 0; i < elements; i++)
-            if (is_sign(outputList[i])) 
-                sign++;
-
-        double temp = elements * 1.0 / 2;
-        temp = round(temp);
-        if ((temp-sign)==1)
-        {
-            cout << "corect enter"<< endl;
-            status = 1;
-        }
-        else
-        {
-            cout << "Incorect enter, Reenter expresion:" << endl;
-            return -1;
-        }
-
-    }
 
     outputList = count_RPN(outputList);
     cout <<infix <<" = " <<outputList[0];
